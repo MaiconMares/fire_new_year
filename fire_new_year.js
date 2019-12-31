a1119c8bdd3fbc34df1f02212f79097b255ea578
Maicon Mares <maiconmaresunb@gmail.com>
2019-12-31 18:34:05 -0300

    Criando matriz de animacao.

--- fire_new_year.js
+++ fire_new_year.js
@@ -1,17 +1,45 @@
 const firePixelsArray = []
-const fireWidth = 2;
-const fireHeight = 3;
+const fireWidth = 10;
+const fireHeight = 10;
 
 function init() {
-
+    createDataStructure();
+    renderAnimation();
 }
 
 function createDataStructure(){
+    const numberPixels = fireWidth * fireHeight;
 
+    
+    for(let i = 0; i < numberPixels; i ++) {
+        firePixelsArray[i] = 0;
+    }
 }
 
 function calculateFirePropagation(){
+    
+}
 
+function renderAnimation(){
+    let html = '<table cellspacing=0 cellpadding=0>'
+    for(let row = 0; row < fireHeight; row ++){
+        html += '<tr>';
+
+        for(let column = 0; column < fireWidth; column ++){
+            var pixelIndex = column + (fireWidth * row);
+            var fireIntensity = firePixelsArray[pixelIndex]
+
+            html += '<td>';
+            html += `<div class="pixel-index">${pixelIndex}</div>`;
+            html += fireIntensity;
+            html +='</td>';
+        }
+
+        html += '</tr>';
+    }
+    html += '</table>';
+
+    document.querySelector('#fire-new-year').innerHTML = html;
 }
 
 init()
