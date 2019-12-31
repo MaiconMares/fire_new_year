02d00d8b256753ace357dc26e297fcb7b047d4e0
Maicon Mares <maiconmaresunb@gmail.com>
2019-12-31 16:37:50 -0300

    Criando estrutura inicial da animação.

---
+++ fire_new_year.js
@@ -0,0 +1,17 @@
+const firePixelsArray = []
+const fireWidth = 2;
+const fireHeight = 3;
+
+function init() {
+
+}
+
+function createDataStructure(){
+
+}
+
+function calculateFirePropagation(){
+
+}
+
+init()
