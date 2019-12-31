4321056317889cc90756edb3918a4c2386a4e946
Maicon Mares <maiconmaresunb@gmail.com>
2019-12-31 19:45:22 -0300

    Implementando funcoes para calcular intensidade de cor para cada pixel.

--- fire_new_year.js
+++ fire_new_year.js
@@ -4,7 +4,9 @@ const fireHeight = 10;
 
 function init() {
     createDataStructure();
+    createFireSource();
     renderAnimation();
+    setInterval(calculateFirePropagation, 1000);
 }
 
 function createDataStructure(){
@@ -17,7 +19,34 @@ function createDataStructure(){
 }
 
 function calculateFirePropagation(){
-    
+    //It takes index of each pixel column by column and not row by row
+    for(let column = 0; column < fireWidth; column ++){
+        for(let row = 0; row < fireHeight; row ++){
+            var pixelIndex = column + ( fireWidth * row );
+
+            updateFireIntensityPerPixel(pixelIndex);
+        }
+    }
+
+    renderAnimation();
+}
+
+function updateFireIntensityPerPixel(currentPixelIndex){
+    //It calculate intensity of pixel below of currentPixelIndex
+    var belowPixelIndex = currentPixelIndex + fireWidth;
+
+    if(belowPixelIndex >= (fireWidth * fireHeight ) ){
+        return
+    }
+
+    const decay = 1;
+    var belowPixelFireIntensity = firePixelsArray[belowPixelIndex];
+
+    //Block negative values
+    var newFireIntensity = 
+    ( belowPixelFireIntensity - decay ) >= 0 ? belowPixelFireIntensity - decay : 0;
+
+    firePixelsArray[currentPixelIndex] = newFireIntensity;
 }
 
 function renderAnimation(){
@@ -40,6 +69,20 @@ function renderAnimation(){
     html += '</table>';
 
     document.querySelector('#fire-new-year').innerHTML = html;
+}
+
+function createFireSource(){
+    //It begins filling the intensity of fire in the end of table
+
+    for(let column = 0; column <= fireWidth; column ++){
+        const overflowPixelIndex = fireWidth * fireHeight;
+        const pixelIndex = (overflowPixelIndex - fireWidth) + column;
+
+        firePixelsArray[pixelIndex] = 36;
+    }
+
+    /* Is possible to simplify pixelIndex equation, like this: pixelIndex = 
+    fireWidth * (1 - (fireHeight) + column) */
 }
 
 init()
